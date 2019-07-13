b47c592f908113a3fd48ae728044cbb534c4e3b8
nakshi24 <52525396+nakshi24@users.noreply.github.com>
2019-07-13 13:48:25 +0530

    Add files via upload

---
+++ Turtle - Red Spiral Square.py
@@ -0,0 +1,12 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:36:03) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import turtle as t
+>>> a1 = t.Turtle()
+>>> a1.pencolor('red')
+>>> for i in range(250):
+	a1.forward(i)
+	a1.left(91)
+
+	
+>>> 
